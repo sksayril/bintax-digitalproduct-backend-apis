d3cc5b6d0ec73297f240d8062f4a991846c33e97
sksayril <sksayril123@gmail.com>
2025-05-19 17:27:03 +0530

    chnages

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3433;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
